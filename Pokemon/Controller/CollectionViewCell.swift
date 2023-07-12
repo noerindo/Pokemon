d1b82fda4aee31d4f463c5aea4ae813421739a64
indah nurindo <67154750+noerindo@users.noreply.github.com>
2023-07-12 11:00:05 +0700

    tes

---
+++ Pokemon/Controller/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  Pokemon
+//
+//  Created by Indah Nurindo on 12/07/2566 BE.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var urlPokemon: UILabel!
+    @IBOutlet weak var namePokemon: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
